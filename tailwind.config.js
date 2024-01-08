08668c46bcbce4ad7ee8f497f128997ffaa481b0
kingola1 <segun8427@gmail.com>
2024-01-08 12:00:44 +0100

    fonts added

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,31 @@
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
 	content: ["./**/*.{html,js}"],
 	theme: {
 		extend: {
 			fontFamily: {
-				grotesk: ["grotesk", "sans-serif"],
+				grotesk: [
+					"TomatoGrotesk-Bold",
+					"TomatoGrotesk-BlackSlanted",
+					"TomatoGrotesk-Bold",
+					"TomatoGrotesk-BoldSlanted",
+					"TomatoGrotesk-ExtraBold",
+					"TomatoGrotesk-ExtraBoldSlanted",
+					"TomatoGrotesk-ExtraLight",
+					"TomatoGrotesk-ExtraLightSlanted",
+					"TomatoGrotesk-Light",
+					"TomatoGrotesk-LightSlanted",
+					"TomatoGrotesk-Medium",
+					"TomatoGrotesk-MediumSlanted",
+					"TomatoGrotesk-Regular",
+					"TomatoGrotesk-SemiBold",
+					"TomatoGrotesk-SemiBoldSlanted",
+					"TomatoGrotesk-Slanted",
+					"TomatoGrotesk-Thin",
+					"TomatoGrotesk-ThinSlanted",
+					"sans-serif",
+				],
 			},
 		},
 	},
